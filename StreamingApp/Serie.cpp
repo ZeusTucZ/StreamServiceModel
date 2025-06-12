0595b104bfc59d42cea3ccae755f842982afc189
ZeusTucZ <lorenzo.orrante@icloud.com>
2025-06-12 16:56:41 -0600

    Update Serie.cpp

--- StreamingApp/Serie.cpp
+++ StreamingApp/Serie.cpp
@@ -65,6 +65,6 @@ void Serie::reproducir() const {
 // NUEVO: Método para reproducir el tráiler
 void Serie::reproducirTrailer() const {
     cout << "Reproduciendo tráiler de la serie \"" << nombre << "\": " << trailerURL << endl;
-    string comando = "open " + trailerURL; // Usa "open" en Mac
+    string comando = "open " + trailerURL; // start en windows y open en mac
     system(comando.c_str());
 }
